3b6224fc664dd22995857d30cf4fd542d8c3222d
Luis Carlos Mencia <luiscarlos@lcmencia.com>
2022-06-10 20:19:46 -0400

    routes

--- models/server.ts
+++ models/server.ts
@@ -1,17 +1,28 @@
 import express, { Application } from 'express';
+import userRoutes from '../routes/user';
+
 class Server {
     
     private app: Application;
     private port: string;
+    private apiPaths = {
+        users: '/api/users'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        this.routes();
+    }
+
+    routes() {
+        this.app.use(this.apiPaths.users, userRoutes)
     }
 
     listen() {
         this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto '+ this.port);
+            console.log(`Servidor corriendo en puerto!!! ${this.port}`);
         })
     }
 }
